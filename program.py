b4cbbea56719e39b5653d9c2478ff470dded97cc
jan972002 <122623380+jan972002@users.noreply.github.com>
2023-01-14 18:17:29 +0100

    Add files via upload

--- program.py
+++ program.py
@@ -1,59 +1,80 @@
-from ast import Return
 import math
+import time
+
+while True:
+    a = int(10)
+    b = int(100)
+    c = int(200)
+    d = int(150)
 
 
-a = int(10)
-b = int(100)
-c = int(200)
-d = int(150)
+    print(str("\n\n\n\n\n\nMożesz wybrać tylko 3000kg produktów z pośród \ngrochu 5zł za opakowanie 10kg\ncukru 200zł za opakowanie 100kg\nkapusty 30zł za opakowanie 200kg\nbambusa 50zł za opakowanie 150kg\n\n\n\n", ))
+    f = int(input("Wybierz ile opakowań   GROCHU   chcesz?  ") )
+    g = int(input("Wybierz ile opakowań   CUKRU   chcesz?  "))
+    i = int(input("Wybierz ile opakowań   KAPUSTY   chcesz?  "))
+    o = int(input("Wybierz ile opakowań   BAMBUSA   chcesz?  "))
 
 
-print(str("\n\n\n\n\n\nMożesz wybrać tylko 3000kg produktów z pośród \ngrochu 5zł za opakowanie 10kg\ncukru 200zł za opakowanie 100kg\nkapusty 30zł za opakowanie 200kg\nbambusa 50zł za opakowanie 150kg\n\n\n\n", ))
-f = int(input("Wybierz ile opakowań   GROCHU   chcesz?  ") )
-g = int(input("Wybierz ile opakowań   CUKRU   chcesz?  "))
-i = int(input("Wybierz ile opakowań   KAPUSTY   chcesz?  "))
-o = int(input("Wybierz ile opakowań   BAMBUSA   chcesz?  "))
+    h = int(5)
+    e = int(200)
+    m = int(30)
+    k = int(50)
+    print("\n\n\n")
+    print(f*a, "kg grochu ", " w cenie", (f*h))
+    print(g*b, "kg cukru ", " w cenie", (g*e))
+    print(i*c, "kg kapusty ", " w cenie", (i*m))
+    print(o*d, "kg bambusa ", " w cenie", (o*k))
 
+    print("\n\n")
 
-h = int(5)
-e = int(200)
-m = int(30)
-k = int(50)
-print("\n\n\n")
-print(f*a, "kg grochu ", " w cenie", (f*h))
-print(g*b, "kg cukru ", " w cenie", (g*e))
-print(i*c, "kg kapusty ", " w cenie", (i*m))
-print(o*d, "kg bambusa ", " w cenie", (o*k))
-
-print("\n\n")
-
-fa = (f*a)
-gb = (g*b)
-ic = (i*c)
-od = (o*d)
+    fa = (f*a)
+    gb = (g*b)
+    ic = (i*c)
+    od = (o*d)
 
 
 
-fh = (f*h)
-ge = (g*e)
-im = (i*m)
-ok = (o*k)
+    fh = (f*h)
+    ge = (g*e)
+    im = (i*m)
+    ok = (o*k)
 
 
-fagbicod = int(fa+gb+ic+od)    #masa
-fhgeimok = int(fh+ge+im+ok)    #cena
+    fagbicod = int(fa+gb+ic+od)    #masa
+    fhgeimok = int(fh+ge+im+ok)    #cena
 
-pro = int(0.9*fhgeimok)
+    pro = int(0.9*fhgeimok)
+
+    if fhgeimok > 2000 and fagbicod <3000:
+        print("Otrzymujesz bonus, -10% wartości zamówienia")
+        print(str("do zapłaty po odliczeniu rabatu",pro, "zł!"))
+        break
+    elif fagbicod >0 and fagbicod<3000:
+        print("Twoje zamówienie o wartości", fhgeimok, "zł", "i masie",fagbicod,"kg", "zostało przyjęte, opłać je przy kasie!")
+        odp = str(input("Czy chcesz dodać kolejne zamówienie? Wpisz T/N  "))
+        if(odp=="T"or odp=="t"):
+                print("\n\nPrzekierowuję do wyboru produktów!")
+                time.sleep(3)
+        if(odp=="N"or odp=="n"):
+                print("\n\nDziękujemy za zakupy, zapraszamy ponownie!\n\n")
+                break
+    else:
+        print("Zamówiłeś",fagbicod, "kg produktów w cenie", fhgeimok, "Masa twojego zamóienia przekracza dopuszczalny limit. \nProsimy złożyć zamówienie ponownie.")
+        time.sleep(5)
 
 
-if fhgeimok > 2000 and fagbicod <3000:
-    print("Otrzymujesz bonus, -10% wartości zamówienia")
-    print(str("do zapłaty po odliczeniu rabatu",pro, "zł!"))
-elif fagbicod >0 and fagbicod<3000:
-    print("Twoje zamówienie o wartości", fhgeimok, "zł", "i masie",fagbicod,"kg", "zostało przyjęte, opłać je przy kasie!")
-else:
-    print("Zamówiłeś",fagbicod, "kg produktów w cenie", fhgeimok, "Masa twojego zamóienia przekracza dopuszczalny limit. \nProsimy złożyć zamówienie ponownie.")
+
+    print("\n\n")
+    
 
 
-print("\n\n")
+
+    
+    
+    
+
+
+
+
+
     #potrzeba dodać pętlę ponownego odtwarzania
